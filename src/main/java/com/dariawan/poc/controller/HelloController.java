f35e3ed029baa2ab664cdf723229c6864e0e9807
teodesson <teodesson@gmail.com>
2014-09-17 00:06:36 +0800

    finally services appear in swagger-ui but base-path seems wrong previously forgot to include <bean class="com.mangofactory.swagger.configuration.SpringSwaggerConfig" />

--- src/main/java/com/dariawan/poc/controller/HelloController.java
+++ src/main/java/com/dariawan/poc/controller/HelloController.java
@@ -2,13 +2,13 @@ package com.dariawan.poc.controller;
 
 import com.dariawan.poc.model.Hello;
 import com.wordnik.swagger.annotations.ApiOperation;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class HelloController {
 
     @RequestMapping(value = "/dariawan/hello/getHello", method = RequestMethod.GET)
@@ -27,7 +27,7 @@ public class HelloController {
      * 
      * @return greetings
      */
-    @RequestMapping(value = "dariawan/hello/getString", method = RequestMethod.GET)
+    @RequestMapping(value = "/dariawan/hello/getString", method = RequestMethod.GET)
     @ApiOperation(httpMethod = "GET", value = "Greetings using Swagger, return String")
     @ResponseBody
     public String sayHelloAsString() {
