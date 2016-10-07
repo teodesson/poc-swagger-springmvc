73fad17c94614c55ace13eecd805b3055804f93b
Desson Ariawan <teodesson@gmail.com>
2016-10-07 15:33:19 +0800

    change to springfox (cont...)

--- src/main/java/com/dariawan/poc/controller/HelloController.java
+++ src/main/java/com/dariawan/poc/controller/HelloController.java
@@ -1,21 +1,27 @@
 package com.dariawan.poc.controller;
 
 import com.dariawan.poc.model.Hello;
+import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-//@Controller
+// @RequestMapping("/")
+@Api(value = "/", description = "apis for Hello")
 public class HelloController {
 
     
-    @RequestMapping(value = "dariawan/hello/getHello", method = RequestMethod.GET)
-    @ApiOperation(httpMethod = "GET", value = "Greetings using Swagger, return Hello")
-    @ResponseBody
+    @RequestMapping(value = "dariawan/hello/getHello", method = RequestMethod.GET,
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    @ApiOperation(value = "Greetings using Swagger, return Hello", notes = "return Hello", response = Hello.class)
+    @ResponseStatus(HttpStatus.OK)
     public Hello sayHello() {
         Hello hello = new Hello();
         hello.setHelloFrom("Dariawan");
@@ -24,14 +30,11 @@ public class HelloController {
     }
     
     /**
-     * using swagger-springmvc 0.8.8 this function still failed to return result in swagger-ui, 
-     * but can pass integration testing
      * 
      * @return greetings
      */
     @RequestMapping(value = "dariawan/hello/getString", method = RequestMethod.GET)
-    @ApiOperation(httpMethod = "GET", value = "Greetings using Swagger, return String")
-    @ResponseBody
+    @ApiOperation(value = "Greetings using Swagger, return String", notes = "return Hello as String", response = String.class)
     public String sayHelloAsString() {
         return "Greetings from Dariawan!";
     }
