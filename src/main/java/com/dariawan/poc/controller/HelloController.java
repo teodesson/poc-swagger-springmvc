10b1406598c64c917150fdfcba2e05f6abb51552
Desson Ariawan <teodesson@gmail.com>
2016-10-10 10:20:17 +0800

    Add function with parameters

--- src/main/java/com/dariawan/poc/controller/HelloController.java
+++ src/main/java/com/dariawan/poc/controller/HelloController.java
@@ -3,12 +3,13 @@ package com.dariawan.poc.controller;
 import com.dariawan.poc.model.Hello;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -26,6 +27,19 @@ public class HelloController {
         Hello hello = new Hello();
         hello.setHelloFrom("Dariawan");
         hello.setHelloMessage("Greetings!");
+        return hello;
+    }
+    
+    @RequestMapping(value = "dariawan/hello/sayHello/{from}/{message}", method = RequestMethod.GET,
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    @ApiOperation(value = "Greetings using Swagger, return Hello", notes = "return Hello", response = Hello.class)
+    @ResponseStatus(HttpStatus.OK)
+    public Hello sayHello(
+            @ApiParam(name = "from", value = "greetings from?", required = true) @PathVariable("from") String from, 
+            @ApiParam(name = "message", value = "greeting message?", required = true) @PathVariable("message") String message) {
+        Hello hello = new Hello();
+        hello.setHelloFrom(from);
+        hello.setHelloMessage(message);
         return hello;
     }
     
